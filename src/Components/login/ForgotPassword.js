5956093bb2e0fa8e01872e67d8695b34f861940f
pang07302 <pang07302@gmail.com>
2021-07-25 18:49:28 +0100

    minor change

--- src/Components/login/ForgotPassword.js
+++ src/Components/login/ForgotPassword.js
@@ -40,15 +40,17 @@ function ForgotPassword(){
         <CenteredContainer>
             <div className="card">
                 <br/>
-                <p style={welcomeStyle}>Forget Password</p>
+                <h2 className="text-center mb-4">Require new password</h2>
+                <br/><br/>
         
                 <input {...email} type="email" className="form-control" ref={emailRef} id="InputEmail" placeholder="Email" aria-describedby="emailHelp" style={formFields} required></input>
                 <br/><br/>
-                <button type="button" onClick={handleResetPassword} style={regStyle}>Confirm</button>
+          
+                <button type="button" className="btn btn-primary" onClick={handleResetPassword} >Confirm</button>
                 <br/>{message}<br/><br/><br/>
                 
 
-                <Link to="/Login" style={logStyle}>Back</Link>
+                <Link to="/Login">Back</Link>
            
                 <br/><br/>
   
@@ -74,28 +76,6 @@ const formFields = {
     padding: "10px 30px"
 }
 
-const logStyle = {
-    backgroundColor: "#00cc00",
-    color: "white",
-    padding: "7px 60px",
-    fontSize: "17px",
-    borderRadius: "5px",
-    cursor: "pointer",
-    fontWeight: "bold",
-    
-    
-  }
-  
-  const regStyle = {
-    backgroundColor: "#ffcc00",
-    padding: "7px 10px",
-    fontSize: "13px",
-    color: "black",
-    borderRadius: "5px",
-    fontWeight: "bold",
-    cursor: "pointer",
-    textDecoration: "none"
-  }
   const welcomeStyle = {
     fontSize: "30px",
     color: "green"
